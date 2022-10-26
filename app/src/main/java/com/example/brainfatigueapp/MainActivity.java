279610a04143626118e6c926b78082fd4c330288
tomhoad03 <tom.hoad03@gmail.com>
2022-10-26 14:40:41 +0100

    Loading screen

--- app/src/main/java/com/example/brainfatigueapp/MainActivity.java
+++ app/src/main/java/com/example/brainfatigueapp/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.brainfatigueapp;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+            startActivity(intent);
+            finish();
+        }, 3000);
     }
 }
