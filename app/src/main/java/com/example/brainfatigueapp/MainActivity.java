344dc227b0e0506613d4ba520de52908d38f73d5
tomhoad03 <tom.hoad03@gmail.com>
2022-10-31 16:14:08 +0000

    Action bar null check

--- app/src/main/java/com/example/brainfatigueapp/MainActivity.java
+++ app/src/main/java/com/example/brainfatigueapp/MainActivity.java
@@ -7,14 +7,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // Hide action bar
-        getSupportActionBar().hide();
+        if (getSupportActionBar() != null)
+            getSupportActionBar().hide();
 
         // Display homepage after delay
         new Handler(Looper.getMainLooper()).postDelayed(() -> {
