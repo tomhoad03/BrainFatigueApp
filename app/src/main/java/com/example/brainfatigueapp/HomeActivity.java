7ee4b6a9368a500084c9b44ddf1c289b7a4ffeaa
tomhoad03 <tom.hoad03@gmail.com>
2022-11-01 11:40:39 +0000

    Added buttons with strings

--- app/src/main/java/com/example/brainfatigueapp/HomeActivity.java
+++ app/src/main/java/com/example/brainfatigueapp/HomeActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class HomeActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // Hide action bar
+        if (getSupportActionBar() != null)
+            getSupportActionBar().hide();
     }
 }
