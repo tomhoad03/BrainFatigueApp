3fb0943c2140db9e26d435650b4c5489bc6eb352
tomhoad03 <tom.hoad03@gmail.com>
2022-11-07 15:12:12 +0000

    Load survey from button

--- app/src/main/java/com/example/brainfatigueapp/HomeActivity.java
+++ app/src/main/java/com/example/brainfatigueapp/HomeActivity.java
@@ -1,7 +1,10 @@
 package com.example.brainfatigueapp;
 
+import android.content.Intent;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.brainfatigueapp.survey.SurveyActivity1;
 
 public class HomeActivity extends AppCompatActivity {
     @Override
@@ -12,5 +15,12 @@ public class HomeActivity extends AppCompatActivity {
         // Hide action bar
         if (getSupportActionBar() != null)
             getSupportActionBar().hide();
+
+        // Start survey button
+        final Button startSurveyBtn = findViewById(R.id.activity_home_survey_button);
+        startSurveyBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(HomeActivity.this, SurveyActivity1.class);
+            startActivity(intent);
+        });
     }
 }
