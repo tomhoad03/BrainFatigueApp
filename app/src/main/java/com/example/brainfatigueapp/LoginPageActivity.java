5db4bcceb5fa83c97a188004782ad558e23be36c
Max-Burgess <41290047+Max-Burgess@users.noreply.github.com>
2022-11-09 21:06:41 +0000

    completed basic layout of login page,

--- app/src/main/java/com/example/brainfatigueapp/LoginPageActivity.java
+++ app/src/main/java/com/example/brainfatigueapp/LoginPageActivity.java
@@ -5,6 +5,11 @@ import android.os.Handler;
 import android.os.Looper;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
+
 public class LoginPageActivity extends AppCompatActivity{
 
     @Override
@@ -16,7 +21,15 @@ public class LoginPageActivity extends AppCompatActivity{
         if (getSupportActionBar() != null)
             getSupportActionBar().hide();
 
+        TextView username = (TextView) findViewById(R.id.username);
+        TextView password = (TextView) findViewById(R.id.password);
 
+        Button loginButton = (Button) findViewById(R.id.login_continue_button);
+        loginButton.setOnClickListener(v -> {
+                    Intent intent = new Intent(LoginPageActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                }
+                );
 
     }
 }
