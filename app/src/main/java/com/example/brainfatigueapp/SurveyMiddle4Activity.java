078a39ed6a1b947b468f4fac26dc4b0893c7d3a7
tomhoad03 <tom.hoad03@gmail.com>
2022-11-09 11:29:33 +0000

    All survey pages

--- app/src/main/java/com/example/brainfatigueapp/SurveyMiddle4Activity.java
+++ app/src/main/java/com/example/brainfatigueapp/SurveyMiddle4Activity.java
@@ -1,32 +1,33 @@
 package com.example.brainfatigueapp;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageButton;
 import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class SurveyMiddle1Activity extends AppCompatActivity {
+public class SurveyMiddle4Activity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_survey_middle1);
+        setContentView(R.layout.activity_survey_middle4);
 
         // Hide action bar
         if (getSupportActionBar() != null)
             getSupportActionBar().hide();
 
         // Back button
-        final ImageButton startSurveyBtn = findViewById(R.id.activity_survey_middle1_back_button);
+        final ImageButton startSurveyBtn = findViewById(R.id.activity_survey_middle2_back_button);
         startSurveyBtn.setOnClickListener(v -> {
-            Intent intent = new Intent(SurveyMiddle1Activity.this, SurveyStartActivity.class);
+            Intent intent = new Intent(SurveyMiddle4Activity.this, SurveyMiddle3Activity.class);
             startActivity(intent);
         });
 
         // Next button
-        final Button surveyNextBtn = findViewById(R.id.activity_survey_middle1_next_button);
+        final Button surveyNextBtn = findViewById(R.id.activity_survey_middle2_next_button);
         surveyNextBtn.setOnClickListener(v -> {
-            Intent intent = new Intent(SurveyMiddle1Activity.this, SurveyEndActivity.class);
+            Intent intent = new Intent(SurveyMiddle4Activity.this, SurveyMiddle5Activity.class);
             startActivity(intent);
         });
     }
--- app/src/main/java/com/example/brainfatigueapp/SurveyMiddle9Activity.java
+++ app/src/main/java/com/example/brainfatigueapp/SurveyMiddle9Activity.java
@@ -1,32 +1,33 @@
 package com.example.brainfatigueapp;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageButton;
 import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class SurveyMiddle1Activity extends AppCompatActivity {
+public class SurveyMiddle9Activity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_survey_middle1);
+        setContentView(R.layout.activity_survey_middle9);
 
         // Hide action bar
         if (getSupportActionBar() != null)
             getSupportActionBar().hide();
 
         // Back button
-        final ImageButton startSurveyBtn = findViewById(R.id.activity_survey_middle1_back_button);
+        final ImageButton startSurveyBtn = findViewById(R.id.activity_survey_middle2_back_button);
         startSurveyBtn.setOnClickListener(v -> {
-            Intent intent = new Intent(SurveyMiddle1Activity.this, SurveyStartActivity.class);
+            Intent intent = new Intent(SurveyMiddle9Activity.this, SurveyMiddle8Activity.class);
             startActivity(intent);
         });
 
         // Next button
-        final Button surveyNextBtn = findViewById(R.id.activity_survey_middle1_next_button);
+        final Button surveyNextBtn = findViewById(R.id.activity_survey_middle2_next_button);
         surveyNextBtn.setOnClickListener(v -> {
-            Intent intent = new Intent(SurveyMiddle1Activity.this, SurveyEndActivity.class);
+            Intent intent = new Intent(SurveyMiddle9Activity.this, SurveyEndActivity.class);
             startActivity(intent);
         });
     }
