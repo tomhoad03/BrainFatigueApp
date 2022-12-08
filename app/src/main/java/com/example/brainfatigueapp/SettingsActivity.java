a8051b824238a1b122e7d8c6b999b2092390b72f
LewisHawkins <35339211+LewisHawkins@users.noreply.github.com>
2022-12-08 16:50:16 +0000

    The 'dark mode' button now toggles the switch

--- app/src/main/java/com/example/brainfatigueapp/SettingsActivity.java
+++ app/src/main/java/com/example/brainfatigueapp/SettingsActivity.java
@@ -2,7 +2,9 @@ package com.example.brainfatigueapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Switch;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceFragmentCompat;
@@ -23,6 +25,15 @@ public class SettingsActivity extends AppCompatActivity {
         settingsBackBtn.setOnClickListener(v -> {
             Intent intent = new Intent(SettingsActivity.this, DashboardActivity.class);
             startActivity(intent);
+        });
+
+        // The dark mode toggles the switch, as well as the switch itself
+        final Button darkModeBtn = findViewById(R.id.activity_settings_button_dark_mode);
+        darkModeBtn.setOnClickListener(v -> {
+            // Toggle the switch to the opposite state
+            final Switch darkModeSwitch = findViewById(R.id.activity_settings_switch_dark_mode);
+            Boolean currentState = darkModeSwitch.isChecked();
+            darkModeSwitch.setChecked(!currentState);
         });
     }
 }
