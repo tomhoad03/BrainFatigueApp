ab2f7967cfa4c7897de70967cd5933a0445da12b
LewisHawkins <35339211+LewisHawkins@users.noreply.github.com>
2022-12-13 11:18:57 +0000

    Began programmatic creation of report boxes

--- app/src/main/java/com/example/brainfatigueapp/DashboardRightFrag.java
+++ app/src/main/java/com/example/brainfatigueapp/DashboardRightFrag.java
@@ -1,6 +1,8 @@
 package com.example.brainfatigueapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,5 +16,27 @@ public class DashboardRightFrag extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_dashboard_right, container, false);
+    }
+
+    @Override
+    public void onViewCreated (View view, Bundle savedInstanceState) {
+        addReportBox(view);
+    }
+
+    private void addReportBox(View view) {
+        // Adds a new report box to the scrollable list
+
+        // Get the layout
+        ConstraintLayout layout = (ConstraintLayout) getView().findViewById(R.id.activity_right_fragment_reports_container);
+
+        // Format the button
+        Button newButton = new Button(getActivity());
+        newButton.setLayoutParams(new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT,
+                ConstraintLayout.LayoutParams.WRAP_CONTENT));
+        newButton.setText("Button");
+        // newButton.setId(111);
+
+        //add button to the layout
+        layout.addView(newButton);
     }
 }
