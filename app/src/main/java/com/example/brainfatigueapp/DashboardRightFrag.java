ec94ab2ddf99dd116093695fa1e81f0c62f75538
LewisHawkins <35339211+LewisHawkins@users.noreply.github.com>
2022-12-14 12:19:21 +0000

    Began adding constraints to report boxes

--- app/src/main/java/com/example/brainfatigueapp/DashboardRightFrag.java
+++ app/src/main/java/com/example/brainfatigueapp/DashboardRightFrag.java
@@ -3,9 +3,11 @@ package com.example.brainfatigueapp;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
@@ -66,26 +68,32 @@ public class DashboardRightFrag extends Fragment {
         System.out.println("CURRENTLY: " + sdf.format(calendar.getTime()));
 
         // Create a new button to add to the page
-        Button newButton = formatButton(sdf.format(calendar.getTime()));
-        layout.addView(newButton);
+        formatButton(sdf.format(calendar.getTime()), layout);
     }
 
-    private Button formatButton (String time) {
+    private void formatButton (String time, ConstraintLayout layout) {
         // This function takes the required data to create a report and makes it look like it should
         Button newButton = new Button(getActivity());
+        layout.addView(newButton);
 
-        // Set the id of the new button so it can be referred to later
-        // newButton.setId(111)
+        // Set the id of the new button so that it can be referred to later
+        newButton.setId((int) 1234);
+        newButton.setTag("button1");
 
         // Apply the drawable for a report box to the button
         newButton.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.report_box));
         newButton.setText(time); // Placeholder
 
         // Align and constrain the button into position
-        // centre in parent
+        ConstraintSet constrain = new ConstraintSet();
+        constrain.clone(layout);
+        constrain.connect(newButton.getId(), ConstraintSet.LEFT,
+                          layout.getId(),    ConstraintSet.LEFT); // ConstraintSet.PARENT_ID / layout.getId() both work
+        constrain.connect(newButton.getId(), ConstraintSet.RIGHT,
+                          layout.getId(),    ConstraintSet.RIGHT);
         // constrain top to bottom of previous box in list, unless it's the first one)
         // set the top margin size to 10dp
+        constrain.applyTo(layout);
 
-        return newButton;
     }
 }
