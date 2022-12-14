0e241f6987d25fbe3d852ed10013a8cf641dbb5a
Tom Hoad <40165392+tomhoad03@users.noreply.github.com>
2022-12-14 15:47:32 +0000

    Fixed notification date and displays result

--- app/src/main/java/com/example/brainfatigueapp/SurveyEndActivity.java
+++ app/src/main/java/com/example/brainfatigueapp/SurveyEndActivity.java
@@ -7,9 +7,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Locale;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -24,6 +27,17 @@ public class SurveyEndActivity extends AppCompatActivity {
         if (getSupportActionBar() != null)
             getSupportActionBar().hide();
 
+        // Schedule the next notification
+        long interval = 10800000L, dayStart = 32400000L, blockStart = 39600000L, blockEnd = 54000000L, dayEnd = 79200000L;
+        long scheduledNotification = scheduleNotification(System.currentTimeMillis(), interval, dayStart, blockStart, blockEnd, dayEnd);
+
+        // Notify the user of the next notification
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm", Locale.UK);
+        String submitPromptString = "To finish, press submit. The next survey prompt will arrive at " + sdf.format(System.currentTimeMillis() + scheduledNotification) + ".";
+
+        final TextView submitPromptText = findViewById(R.id.endSurveySubMessage);
+        submitPromptText.setText(submitPromptString);
+
         // Next button
         final Button surveySubmitBtn = findViewById(R.id.activity_survey_end_submit_button);
         surveySubmitBtn.setOnClickListener(v -> {
@@ -37,15 +51,12 @@ public class SurveyEndActivity extends AppCompatActivity {
             });
             executorService.shutdown();
 
-            // Schedule the next notification
-            long interval = 10800000L, dayStart = 32400000L, blockStart = 39600000L, blockEnd = 54000000L, dayEnd = 79200000L;
-            long scheduledNotification = scheduleNotification(System.currentTimeMillis(), interval, dayStart, blockStart, blockEnd, dayEnd);
-
             // Setup the next notification
             Intent notifyIntent = new Intent(getApplicationContext(), NotificationReceiver.class);
             PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
             AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-            alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, scheduledNotification, pendingIntent);
+            alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + scheduledNotification, pendingIntent);
+            Log.d("notification_scheduled", sdf.format(System.currentTimeMillis() + scheduledNotification));
 
             Intent intent = new Intent(SurveyEndActivity.this, HomeActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
