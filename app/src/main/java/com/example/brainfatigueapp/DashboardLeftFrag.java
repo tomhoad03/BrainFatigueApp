dff6b62936b8f6ee479f2cd1775b459e5ed4f17a
LewisHawkins <35339211+LewisHawkins@users.noreply.github.com>
2022-12-16 14:31:56 +0000

    Popups now also appear in the left fragment

--- app/src/main/java/com/example/brainfatigueapp/DashboardLeftFrag.java
+++ app/src/main/java/com/example/brainfatigueapp/DashboardLeftFrag.java
@@ -1,10 +1,23 @@
 package com.example.brainfatigueapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.fragment.app.FragmentManager;
+
+import java.text.SimpleDateFormat;
+import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
+import java.util.concurrent.*;
 
 public class DashboardLeftFrag extends Fragment {
 
@@ -13,5 +26,116 @@ public class DashboardLeftFrag extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_dashboard_left, container, false);
+    }
+
+    @Override
+    public void onViewCreated (View view, Bundle savedInstanceState) {
+        // Add report boxes with data from the database to the scrollable list
+
+        // Get the layout
+        ConstraintLayout layout = getView().findViewById(R.id.activity_left_fragment_reports_container);
+
+        // Get the data from the database
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        Future<List<SurveyResult>> surveyResultsFuture = executorService.submit(() -> {
+            SurveyDatabase surveyDatabase = SurveyDatabase.getDatabase(getContext().getApplicationContext());
+            SurveyResultDao surveyResultDao = surveyDatabase.surveyResultDao();
+
+            return surveyResultDao.getAll();
+        });
+        executorService.shutdown();
+
+        List<SurveyResult> surveyResults = null;
+        try {
+            surveyResults = surveyResultsFuture.get(200, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            e.printStackTrace();
+        }
+        // Log.d("survey_results", surveyResults.toString());
+
+        // Fill the fragment with a report box for every entry in the database
+        int boxCount = 0;
+        for (SurveyResult nextResult : surveyResults) {
+            // Create a report box for this survey result
+            formatButton(nextResult, boxCount, layout);
+            boxCount++;
+        }
+    }
+
+    private void formatButton (SurveyResult result, int count, ConstraintLayout layout) {
+        // Set the strings for the text
+        SimpleDateFormat sdf = new SimpleDateFormat("EEE MMM d, hh:mmaaa", Locale.UK);
+        GregorianCalendar calendar = new GregorianCalendar(TimeZone.getTimeZone("Europe/London"));
+        calendar.setTimeInMillis(result.getSurveyResultId());
+        System.out.println("CURRENTLY: " + sdf.format(calendar.getTime()));
+        String mainText = sdf.format(calendar.getTime());
+        String subText = "You reported a fatigue level of " + result.getQuestion1().toString() + "."; // Update to new string method
+
+        // This function takes the required data to create a report and makes it look like it should
+        Button newButton = new Button(getActivity());
+        layout.addView(newButton);
+
+        // Set the id of the new button so that it can be referred to later
+        newButton.setId(10000000 + count);
+
+        // Apply the drawable for a report box to the button
+        newButton.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.report_box));
+
+        // Align and constrain the button into position
+        ConstraintSet constrain = new ConstraintSet();
+        constrain.clone(layout);
+        // Horizontally
+        constrain.connect(newButton.getId(), ConstraintSet.LEFT,
+                layout.getId(),    ConstraintSet.LEFT); // ConstraintSet.PARENT_ID / layout.getId() both work
+        constrain.connect(newButton.getId(), ConstraintSet.RIGHT,
+                layout.getId(),    ConstraintSet.RIGHT);
+        // Vertically
+        // If statement here, if not the first box -> set the top of box to the bottom of the previous box
+        if (count == 0) {
+            constrain.connect(newButton.getId(), ConstraintSet.TOP,
+                    layout.getId(), ConstraintSet.TOP); // Constrain the box to the top of the layout
+        } else {
+            constrain.connect(newButton.getId(), ConstraintSet.TOP,
+                    (int) newButton.getId() - 1, ConstraintSet.BOTTOM); // Constrain the box to the bottom of the previous box
+        }
+        constrain.setMargin(newButton.getId(), ConstraintSet.TOP, 60);
+        constrain.applyTo(layout);
+
+        // Set the functionality of the buttons so that they open a popup when clicked
+        newButton.setOnClickListener(v -> {
+            FragmentManager manager = getFragmentManager();
+            DashboardPopupFrag popup = new DashboardPopupFrag(result, mainText);
+            popup.show(manager, "popup");
+        });
+
+        // Create the text that goes in the report boxes (styles applied in constructor)
+        TextView newMainText = new TextView(getActivity(), null, 0, R.style.report_box_main_text);
+        layout.addView(newMainText);
+        TextView newSubText = new TextView(getActivity(), null, 0, R.style.report_box_sub_text);
+        layout.addView(newSubText);
+
+        // Give the texts ids
+        newMainText.setId(20000000 + count);
+        newSubText.setId(30000000 + count);
+
+        ConstraintSet constrainText = new ConstraintSet();
+        constrainText.clone(layout);
+
+        newMainText.setText(mainText); // Time could be formatted differently
+        newSubText.setText(subText);
+
+        newMainText.bringToFront();
+        newSubText.bringToFront();
+
+        // Set the constraints on the text
+        constrainText.connect(newMainText.getId(), ConstraintSet.TOP,
+                newButton.getId(),   ConstraintSet.TOP, 28);
+        constrainText.connect(newMainText.getId(), ConstraintSet.LEFT,
+                newButton.getId(),   ConstraintSet.LEFT, 36);
+        constrainText.connect(newSubText.getId(),  ConstraintSet.TOP,
+                newMainText.getId(), ConstraintSet.BOTTOM, 16);
+        constrainText.connect(newSubText.getId(),  ConstraintSet.LEFT,
+                newMainText.getId(),   ConstraintSet.LEFT);
+        constrainText.applyTo(layout);
     }
 }
