60fb2ec03bb55cb579f7c6e1cf922d81c904eb61
Max-Burgess <41290047+Max-Burgess@users.noreply.github.com>
2023-01-04 14:22:02 +0000

    added fitbit login page

---
+++ app/src/main/java/com/example/brainfatigueapp/FitBitLoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.brainfatigueapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FitBitLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fit_bit_login);
+    }
+}
