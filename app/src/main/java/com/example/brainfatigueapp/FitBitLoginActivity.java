ba44456996f2fb196288d09cdc8c26e99eb0e435
Max-Burgess <41290047+Max-Burgess@users.noreply.github.com>
2023-01-05 17:09:03 +0000

    added login button functionality

--- app/src/main/java/com/example/brainfatigueapp/FitBitLoginActivity.java
+++ app/src/main/java/com/example/brainfatigueapp/FitBitLoginActivity.java
@@ -2,7 +2,10 @@ package com.example.brainfatigueapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class FitBitLoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class FitBitLoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fit_bit_login);
+
+        // Hide action bar
+        if (getSupportActionBar() != null)
+            getSupportActionBar().hide();
+
+        // Load fitbit login page
+        Button startSurveyBtn = findViewById(R.id.fitbit_login_button);
+        startSurveyBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=239457&scope=activity+cardio_fitness+electrocardiogram+heartrate+location+nutrition+oxygen_saturation+profile+respiratory_rate+settings+sleep+social+temperature+weight"));
+            startActivity(intent);
+        });
     }
 }
