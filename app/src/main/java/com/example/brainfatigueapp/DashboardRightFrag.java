d73ce726de25c63be3af54d2c876c48f80acde10
Max Burgess <41290047+Max-Burgess@users.noreply.github.com>
2023-01-13 11:13:45 +0000

    Updating fitbit labels (#63)
    * added buttons to fitbit dashboard
    
    * added in some example data for the fitbit graphs so people can still see it if they dont havea  fitbit account.
    
    * fitxed fitbit labels

--- app/src/main/java/com/example/brainfatigueapp/DashboardRightFrag.java
+++ app/src/main/java/com/example/brainfatigueapp/DashboardRightFrag.java
@@ -148,6 +148,7 @@ public class DashboardRightFrag extends Fragment {
         }
         ArrayList<Entry> chartData = new ArrayList<>();
         //add the long term lifetime data to the chart
+        /*
         new Thread(() -> {
             fitBitAPIHandler.parseUserActivities(fitBitAPIHandler.getUserActivities());
             HashMap<String, DaysActivity> activities = fitBitAPIHandler.getActivitySummaries();
@@ -157,6 +158,13 @@ public class DashboardRightFrag extends Fragment {
                 i++;
             }
         }).start();
+
+         */
+        chartData.add(new Entry(0, 0));
+        chartData.add(new Entry(1, 30));
+        chartData.add(new Entry(2, 60));
+        chartData.add(new Entry(3, 45));
+        chartData.add(new Entry(4, 75));
         return chartData;
     }
 
@@ -304,7 +312,7 @@ public class DashboardRightFrag extends Fragment {
     private void drawFitbitGraph(LineChart lineChart3) {
         // Apply fitbit data from the database to the graph
         ArrayList<Entry> data = getFitbitData();
-        LineDataSet lineChartData3 = new LineDataSet(data, "(fitbit data)");
+        LineDataSet lineChartData3 = new LineDataSet(data, "Minutes of exercise per day");
         ArrayList<ILineDataSet> iLineDataSets3 = new ArrayList<ILineDataSet>();
         iLineDataSets3.add(lineChartData3);
         LineData lineData3 = new LineData(iLineDataSets3);
@@ -324,7 +332,7 @@ public class DashboardRightFrag extends Fragment {
         float axisMax = getLargestDatapoint(data);
         axisMax = (float) (100 * (Math.ceil(Math.abs(axisMax / 100)))); // Round up to the nearest a hundred
         yLeft.setAxisMaximum(axisMax);
-        yLeft.setLabelCount(4);
+        yLeft.setLabelCount(5);
 
         // Set the data and update
         lineChart3.setData(lineData3);
